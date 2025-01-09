fcf67e6ebe24ae825bf6e68dacdc8219191c21f2
Biswas Khayargoli <biswas.khayargoli@gmail.com>
2025-01-09 18:45:55 +0545

    latest

--- vite.config.js
+++ vite.config.js
@@ -29,12 +29,12 @@ export default defineConfig({
         short_name: 'BeeU',
         icons: [
           {
-            src: '/icons/icon-192.webp',
+            src: '/icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icons/icon-512.webp',
+            src: '/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
