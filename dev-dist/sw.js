a5d23d5b84b9b42643105786e1d2819b0f4d5124
Biswas Khayargoli <biswas.khayargoli@gmail.com>
2025-01-09 19:03:23 +0545

    latest

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s5gleeqvk8"
+    "revision": "0.a8797bmbtg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
