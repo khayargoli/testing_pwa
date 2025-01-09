8df453d792abb8afff1f07e39b32ab24c5914d50
Biswas Khayargoli <biswas.khayargoli@gmail.com>
2025-01-09 20:08:43 +0545

    new

---
+++ public/vite.config.js
@@ -0,0 +1,62 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    headers: {
+      'Cache-Control': 'no-store',
+      Pragma: 'no-cache',
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  plugins: [
+    react(),
+    VitePWA({  
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'BeeU',
+        short_name: 'BeeU',
+        description: 'BeeU - pollinate your next level social video clip network',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "icon-192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "src": "icon-512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        start_url: '/',
+        theme_color: '#f9b400',
+        background_color: '#c3e6f8',
+        display: 'standalone',
+        scope: '/'
+      },
+    }),
+  ],
+});
