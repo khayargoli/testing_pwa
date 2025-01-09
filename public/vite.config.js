8daaacf2438c39042617e4820683fed1cfd0836e
Biswas Khayargoli <biswas.khayargoli@gmail.com>
2025-01-09 21:27:26 +0545

    new

--- public/vite.config.js
+++ public/vite.config.js
@@ -29,23 +29,23 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            "src": "icon-192.png",
-            "type": "image/png",
-            "sizes": "192x192"
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            "src": "icon-512.png",
-            "type": "image/png",
-            "sizes": "512x512"
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
           {
-            src: 'icon-512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'icon-512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
